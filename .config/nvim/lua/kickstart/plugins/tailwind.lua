c5ada5be0b9da3132582d620dffccaa5787be764
Tim Littlemore <tim@tjl2.com>
2024-07-09 22:02:40 +0100

    nvim: autopairs and tailwind

---
+++ .config/nvim/lua/kickstart/plugins/tailwind.lua
@@ -0,0 +1,8 @@
+-- tailwind-tools.lua
+return {
+  'luckasRanarison/tailwind-tools.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    custom_filetypes = { 'heex' },
+  },
+}
