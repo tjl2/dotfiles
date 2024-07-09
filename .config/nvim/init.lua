defc572cd00b103b45c66d293b35e117a1954ea8
Tim Littlemore <tim.littlemore+gh@dice.fm>
2024-07-09 15:01:00 +0100

    nvim: add catppuccin theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -240,6 +240,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'catppuccin/nvim', as = 'catppuccin' }, -- catppuccin theme
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -805,7 +806,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
