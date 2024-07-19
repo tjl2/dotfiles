c43396be790a1befbf68cb7c3bed6d8e606d1605
Tim Littlemore <tim@tjl2.com>
2024-07-19 11:00:56 +0100

    lazyvim: add ';' for command mode

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Allow ; to be used for entering command mode
+vim.keymap.set({ "n" }, ";", ":", { desc = "Command mode", noremap = false })
