d0f841f26ef2960a0551539366d8e1a6586846f2
Tim Littlemore <tim.littlemore+gh@dice.fm>
2024-08-08 10:53:27 +0100

    next-ls not working as expected, go back to elixir-ls

--- .config/lazyvim/lua/plugins/elixir-tools.lua
+++ .config/lazyvim/lua/plugins/elixir-tools.lua
@@ -1,21 +1,22 @@
-return {
-  "elixir-tools/elixir-tools.nvim",
-  version = "*",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local elixir = require("elixir")
-
-    elixir.setup({
-      nextls = { enable = true },
-      elixirls = {
-        enable = false,
-      },
-      projectionist = {
-        enable = true,
-      },
-    })
-  end,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-}
+return {}
+-- return {
+--   "elixir-tools/elixir-tools.nvim",
+--   version = "*",
+--   event = { "BufReadPre", "BufNewFile" },
+--   config = function()
+--     local elixir = require("elixir")
+--
+--     elixir.setup({
+--       nextls = { enable = true },
+--       elixirls = {
+--         enable = false,
+--       },
+--       projectionist = {
+--         enable = true,
+--       },
+--     })
+--   end,
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+-- }
