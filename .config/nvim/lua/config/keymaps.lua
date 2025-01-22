0d20a18ddbb98a5da2595ac7187a5f0e08ef755c
Tim Littlemore <tim@tjl2.com>
2025-01-22 21:22:41 +0000

    kickstart.nvim

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- Allow ; to be used for entering command mode
-vim.keymap.set({ "n" }, ";", ":", { desc = "Command mode", noremap = false })
